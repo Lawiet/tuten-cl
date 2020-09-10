7bea6e3a5df713f167d53ea5983f4a476bcfcb00
Jorge Gonzalez <ljorgelgonzalez@gmail.com>
2020-09-10 15:42:24 -0400

    Reparacion de url y del almacenado de la información

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.0.
 
+````
+  "engines": {
+    "@angular/cli": "10.1.0",
+    "node": "12.16.2",
+    "npm": "6.14.4"
+  }
+````
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/principal/login/login.component.ts
+++ src/app/principal/login/login.component.ts
@@ -3,7 +3,6 @@ import {FormGroup, FormGroupDirective} from '@angular/forms';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {MatSnackBar} from '@angular/material/snack-bar';
-import {User} from '../../entity/user';
 import {Router} from '@angular/router';
 
 @Component({
@@ -28,29 +27,20 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
-    this.oHttpClient.put(environment.urlServer + '#!/user/' + this.model.email, {}, {
+    this.oHttpClient.put(environment.urlServer + 'rest/user/' + this.model.email, {}, {
       headers: {
-        'X-Requested-With': 'Accept',
         'Content-Type': 'application/json',
         Accept: 'application/json',
         app: 'APP_BCK',
         password: this.model.password,
       },
     }).subscribe((data: HttpResponse<any>) => {
-      const user = new User().deepCopy(data.body);
-      sessionStorage.setItem('user', data.body);
+      sessionStorage.setItem('user', JSON.stringify(data));
       this.oRouter.navigateByUrl('/booking');
     }, (err) => {
       console.error(err);
       this.openSnackBar('Usuario y/o contraseña incorrecta');
-      this.temp();
     });
-  }
-
-  temp() {
-    const user = '{"serviceData":null,"userAvailability":null,"sessionTokenBck":"testapis@tuten.clp8o4kug7f7n4avm30b2g30135p","firstName":"admin","lastName":"de lectura","email":"testapis@tuten.cl","active":true,"passwordHash":null,"sessionTokenWeb":null,"phoneNumber":"123456","agreedToTermsOfUse":true,"whereKnownUs":null,"lastLogin":1599756400940,"sessionTokenCli":null,"sessionTokenPro":null,"funds":0.0,"tokenFacebook":null,"tokenGoogle":null,"tokensIonic":null,"photoPath":null,"photoExt":null,"userRole":{"userRole":"ADMIN","description":"","fatherUserRole":null,"domain":"tuten","estatus":null,"defaultNamespace":null,"id":1},"sync":0,"usedCodeList":"","referrer":"","rut":null,"domain":"tuten","typeProfessional":null,"tutenSubRole":null,"userId":null,"appVersion":null,"estatus":null}';
-    sessionStorage.setItem('user', user);
-    this.oRouter.navigateByUrl('/booking');
   }
 
   openSnackBar(message: string) {
